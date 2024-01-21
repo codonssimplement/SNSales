c0954b508386928de768380554a5f993be0be6c3
codonssimplement <codons.simplement@gmail.com>
2024-01-21 17:01:53 +0000

    2 commit

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
-app.use("/whatsapp", apiRoute);
+app.use("/", apiRoute);
 
 app.listen(PORT, ()=> (console.log("Le serveur écoute sur le port:"+ PORT)));
